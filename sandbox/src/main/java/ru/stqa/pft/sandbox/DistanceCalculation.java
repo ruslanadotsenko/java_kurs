2f1ab65d47c73dc136c9070f3145e2b93ac1899a
Ruslana <ruslana.dotsenko@devellar.com>
2016-08-31 19:26:42 +0300

    перенесла метод в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceCalculation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceCalculation.java
@@ -9,14 +9,14 @@ class DistanceCalculation {
         Point p1 = new Point(1, 2);
         Point p2 = new Point(4, 6);
 
-        DistanceCalculation.distance(p1,p2);
+        Point.distance(p1,p2);
     }
 
-    public static double distance(Point p1, Point p2)
+   /* public static double distance(Point p1, Point p2)
     {
         double distance = Math.sqrt(Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2));
         System.out.println(" Расстояние между точками p1 и p2 " + " = " + distance);
 
         return distance;
-    }
+    }*/
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,6 +7,14 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public static double distance(Point p1, Point p2)
+    {
+        double distance = Math.sqrt(Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2));
+        System.out.println(" Расстояние между точками p1 и p2 " + " = " + distance);
+
+        return distance;
+    }
 }
 
 
