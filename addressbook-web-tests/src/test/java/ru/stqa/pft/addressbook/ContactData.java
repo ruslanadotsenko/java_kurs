a622b982cff802e0dc749747d89a44ac5000b353
Ruslana <ruslana.dotsenko@devellar.com>
2016-09-17 23:43:46 +0300

    Создан тест для создания нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String email;
+    private final String homephone;
+    private final String mobilephone;
+
+    public ContactData(String firstname, String lastname, String address, String email, String homephone, String mobilephone) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.email = email;
+        this.homephone = homephone;
+        this.mobilephone = mobilephone;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public String getMobilephone() {
+        return mobilephone;
+    }
+}
